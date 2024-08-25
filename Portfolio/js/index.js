57f99d000e14bf90156d547cb44ce11241776bf6
Vidhyanshirajput <145047197+Vidhyanshirajput@users.noreply.github.com>
2024-08-25 19:56:12 +0530

    Delete Portfolio directory

--- Portfolio/js/index.js
+++
@@ -1,9 +0,0 @@
-const toggleNavbar = () => {
-    const navbar = document.querySelector('.navbar');
-    if (navbar) {
-        navbar.style.display = navbar.style.display === 'none' ? 'block' : 'none';
-    }
-};
-
-// Add event listener to the menu icon
-document.querySelector('#menu-icon')?.addEventListener('click', toggleNavbar);
